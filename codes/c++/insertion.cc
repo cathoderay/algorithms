702785f7818154f6d8e5d88690e9ea255eade88e
Ronald Kaiser <raios.catodicos@gmail.com>
2015-09-05 13:56:17 -0300

    reducing number of operations in insertion sort (c++ version)

--- codes/c++/insertion.cc
+++ codes/c++/insertion.cc
@@ -4,36 +4,50 @@
 
 
 void insertion_sort(int v[], int length) {
-    int tmp, p;
-    for(int i = 1; i < length; i++) {
-        p = i;
-        while (p > 0 && v[p] < v[p - 1]) {
-            tmp = v[p];
-            v[p] = v[p - 1];
-            v[p - 1] = tmp;
-            p--;
+    int k, i;
+    for(int j = 1; j < length; j++) {
+        k = v[j];
+        i = j - 1;
+        while (i > -1 && v[i] > k) {
+            v[i + 1] = v[i];
+            i--;
         }
+        v[i + 1] = k;
     }
 }
 
 int main() {
     int v[] = {5, 1, 3, 4, 1};
 
-    insertion_sort(v, 5);
-
     //testing
-    for(int i = 0; i < 5; i++) std::cout << v[i];
+    //insertion_sort(v, 5);
+    //for(int i = 0; i < 5; i++) std::cout << v[i];
 
-
+    
+    /*measuring time*/
     int k = 100000;
     int t[k];
     srand(time(NULL));
-    for(int i = 0; i < k; i++)
-        t[k] = int(rand()*k);
 
-    clock_t start = clock();
+    //init
+    for(int i = 0; i < k; i++)
+        t[i] = i;
+
+    //shuffle
+    int pos, tmp;
+    for(int i = 0; i < k; i++) {
+        pos = rand()%k;
+        tmp = t[i];
+        t[i] = t[pos];
+        t[pos] = tmp;
+    }
+    
+    //go! 
+    clock_t time = clock();
     insertion_sort(t, k);
-    std::cout << (clock() - start) << '\n'; 
+    time = clock() - time;
+    std::cout << (double)(time)/CLOCKS_PER_SEC << " seconds\n"; 
+   
 
     return 0;
 }
