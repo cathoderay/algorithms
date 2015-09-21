d3dd213b96d61a241f2dcaf45b59b22f586b7b52
Ronald Kaiser <raios.catodicos@gmail.com>
2015-09-21 14:32:42 -0300

    improving timeit to accept different kinds of sequences via command line args

--- codes/c++/timeit.cc
+++ codes/c++/timeit.cc
@@ -4,21 +4,36 @@
 
 using namespace std;
 
-#include "helper.h"
-#include "insertion.h"
-#include "selection.h"
-#include "merge.h"
-#include "quick.h"
-#include "heap.h"
+#include "sorts/sort.h"
 
 
-void time() {
-    for(int n = 100; n <= 100000; n+= 100){
+void generate_nums(int a[], int type, int n) {
+    switch (type) {
+        case 0: //random sequences
+            range(a, n);
+            shuffle(a, n);
+            break;
+        case 1: //sorted
+            range(a, n);
+            break;
+        case 2: //reversed
+            reverse(a, n);
+            break;
+        case 3: //lot of repetitions
+            randrept(a, n, 0.8);
+            break;
+        case 4: //few repetitions
+            randrept(a, n, 0.2);
+            break;
+    }
+}
+
+void times(int type, int step) {
+    cout << "n,selection,insertion,merge,quick,heap\n";
+    for(int n = step; n <= 100000; n+= step){
         int a[n];
 
-        srand(time(NULL));
-        range(a, n);
-        shuffle(a, n);
+        generate_nums(a, type, n);
 
         cout << n << ',';
         cout << timeit(selection, a, n) << ',';
@@ -30,8 +45,24 @@ void time() {
 }
 
 
-int main() {
-    time();
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        cout << "Usage: timeit type [step]\n";
+        cout << "types: 0-random,1-sorted,2-reversed,3-lot of rep.,4-few rep.\n";
+        exit(-1);
+    }
+    else {
+        int arg = atoi(argv[1]);
+        if (arg < 0 || arg > 4) {
+            cout << "Invalid argument.";
+            exit(-1);
+        }
+        srand(time(NULL));
+        if (argc == 3)
+            times(arg, atoi(argv[2]));
+        else
+            times(arg, 10000);
+    }
 
     return 0;
 }
