ce9fbbd8e5bd0503a2caa405b8b887cdac996b78
Ronald Kaiser <raios.catodicos@gmail.com>
2015-09-21 17:40:16 -0300

    adding helpers to sort strings

--- codes/c++/sorts/helper.h
+++ codes/c++/sorts/helper.h
@@ -4,11 +4,19 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <string.h>
 #include <time.h>
 
 
 void swap(int *a, int *b) {
    int t;
+   t = *a;
+   *a = *b;
+   *b = t; 
+}
+
+void swap_s(string *a, string *b) {
+   string t;
    t = *a;
    *a = *b;
    *b = t; 
@@ -23,10 +31,17 @@ void print(int *a, int size) {
 }
 
 
+void print_s(string *a, int size) {
+    std::cout << a[0];
+    for(int i=1; i < size; i++)
+        std::cout << ',' << a[i];
+    std::cout << '\n';
+}
+
+
 void copy(int *d, int *s, int size){
     memcpy(d, s, size*sizeof(int));
 }
-
 
 void range(int *a, int size) {
     for(int i = 0; i < size; i++)
@@ -68,12 +83,27 @@ bool is_sorted(int *a, int size) {
     return true;
 }
 
+bool is_sorted_s(string *a, int size) {
+    for(int i = 1; i < size; i++)
+        if (a[i] < a[i-1])
+            return false;
+    return true;
+}
 
-double check_if_sorts(void (*sorter)(int *, int), int*a, int size) {
+
+double check_if_sorts(void (*sorter)(int *, int), int *a, int size) {
     int b[size];
     copy(b, a, size);
     sorter(b, size);
     return is_sorted(b, size);
+}
+
+
+double check_if_sorts_s(void (*sorter)(string *, int), string *a, int size) {
+    string b[size];
+    std::copy(a, a + size, b);
+    sorter(b, size);
+    return is_sorted_s(b, size);
 }
 
 
