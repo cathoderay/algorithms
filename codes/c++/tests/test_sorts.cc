93611098e75e4396a145a789ec4866ab3d5a7447
Ronald Kaiser <raios.catodicos@gmail.com>
2015-09-21 17:43:47 -0300

    adding sorters tests

--- codes/c++/tests/test_sorts.cc
+++ codes/c++/tests/test_sorts.cc
@@ -7,11 +7,12 @@ using namespace std;
 
 
 void run_tests() {
+    int tests = 10;
     int passed = 0;
-    int tests = 5;
+
+    //testing sorts for random seq of ints
     int n = 1000;
     int a[n];
-
     range(a, n);
     shuffle(a, n);
     if (check_if_sorts(selection, a, n)) passed++;
@@ -19,6 +20,15 @@ void run_tests() {
     if (check_if_sorts(merge, a, n)) passed++;
     if (check_if_sorts(quick, a, n)) passed++;
     if (check_if_sorts(heap, a, n)) passed++;
+
+    //testing sorts for strings
+    n = 7;
+    string b[] = {"z", "a", "ba", "aa", "bb", "de", "aa"};
+    if (check_if_sorts_s(selection_s, b, n)) passed++;
+    if (check_if_sorts_s(insertion_s, b, n)) passed++;
+    if (check_if_sorts_s(merge_s, b, n)) passed++;
+    if (check_if_sorts_s(quick_s, b, n)) passed++;
+    if (check_if_sorts_s(heap_s, b, n)) passed++;
 
     cout << "Passed: " << passed << "\n";
     cout << "Total: " << tests << "\n";
